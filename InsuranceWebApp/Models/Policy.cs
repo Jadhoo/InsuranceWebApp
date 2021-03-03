309e9ce93de44fc2bc7d47e449807a5251d26157
Jadhoo <apvaishak9@gmail.com>
2021-03-03 10:15:27 +0530

    Add policy model

---
+++ InsuranceWebApp/Models/Policy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceWebApp.Models
+{
+    public class Policy
+    {
+        public int PolicyNumber { get; set; }
+        public int PlanNumber { get; set; }
+        public string PolicyName { get; set; }
+        public double InstallmentPremium { get; set; }
+        public int InsuredId { get; set; }
+        public string InsuredName { get; set; }
+        public double SumAssured { get; set; }
+        public string PolicyStatus { get; set; }
+        public string PremiumMode { get; set; }
+        public DateTime PremiumDueDate { get; set; }
+        public int BeneficiaryId { get; set; }
+        public string BeneficiaryName { get; set; }
+        public int OwnerId { get; set; }
+        public string OwnerName { get; set; }
+        public int PolicyTerm { get; set; }
+    }
+}
